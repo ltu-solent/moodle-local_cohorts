3df0944f288f5476a76eb3289a7b6387676b37d0
Mark Sharp <mark.sharp@solent.ac.uk>
2024-06-21 10:00:25 +0100

    Bump version

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2024041903;
+$plugin->version  = 2024062100;
 $plugin->requires = 2015111603;
 $plugin->component = 'local_cohorts';
 $plugin->dependencies = [

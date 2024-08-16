438ae3830d43d41edc4e79cd7de6f521e3693823
Mark Sharp <mark.sharp@solent.ac.uk>
2024-08-16 14:28:20 +0100

    Cohort member id was overwriting the user id

--- classes/tables/cohorts_members_table.php
+++ classes/tables/cohorts_members_table.php
@@ -65,10 +65,10 @@ class cohorts_members_table extends table_sql {
         $this->define_columns($columns);
         $this->define_headers($columnheadings);
         $this->define_baseurl(new moodle_url("/local/cohorts/members.php", ['cohortid' => $filters['cohortid']]));
-        $including = ['auth', 'username', 'institution', 'department', 'lastlogin'];
+        $including = ['id', 'auth', 'username', 'institution', 'department', 'lastlogin'];
         $userfieldsapi = \core_user\fields::for_identity(context_system::instance(), false)->with_name()->including(...$including);
         $userfields = $userfieldsapi->get_sql('u', false, '', $this->useridfield, false)->selects;
-        $fields = 'cm.id, cm.timeadded, ' . $userfields;
+        $fields = 'cm.timeadded, ' . $userfields;
         $from = "{cohort_members} cm
             JOIN {user} u ON u.id = cm.userid";
         $where = 'cm.cohortid = :cohortid ORDER BY u.firstname, u.lastname';
